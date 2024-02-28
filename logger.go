9df6d23b8ea9bedc82848d66268f379782cc128d
Andrew <amh11706@gmail.com>
2024-02-28 22:09:36 +0100

    Log errors to stderr

--- logger.go
+++ logger.go
@@ -2,18 +2,20 @@ package logger
 
 import (
 	"log"
+	"os"
 	"runtime/debug"
 )
 
 var infoColor = []interface{}{"\x1b[32m[INFO]\x1b[0m"}
 var errorColor = []interface{}{"\x1b[31m[ERROR]\x1b[0m"}
+var errorPrinter = log.New(os.Stderr, "", log.LstdFlags)
 
 func Info(args ...interface{}) {
 	log.Println(append(infoColor, args...)...)
 }
 
 func Error(args ...interface{}) {
-	log.Println(append(errorColor, args...)...)
+	errorPrinter.Println(append(errorColor, args...)...)
 }
 
 func CheckStack(err error) bool {
